8b565a3f4a0966e6805c33f0600eb9a29b389444
Jim Barntish <jbarntish@axian.com>
2020-05-19 09:39:53 -0700

    Initial code commit.

---
+++ src/PackageTest1/PackageTest1/TestObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PackageTest1
+{
+    public class TestObject
+    {
+        public string HelloWorld()
+        {
+            return "Hello world from package 1!";
+        }
+    }
+}
